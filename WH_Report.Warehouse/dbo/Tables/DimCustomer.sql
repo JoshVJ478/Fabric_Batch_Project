d62eef8a7af51e7e33161507a068c1d359a04cbe
JoshVJ478 <josue.villegas@greybearddynamics.com>
2025-04-30 12:31:41 -0500

    Delete WH_Report.Warehouse directory

--- WH_Report.Warehouse/dbo/Tables/DimCustomer.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[DimCustomer] (
-
-	[CustomerKey] int NULL, 
-	[CustomerSourceID] varchar(8000) NULL, 
-	[CustomerName] varchar(8000) NULL, 
-	[CustomerSegment] varchar(8000) NULL
-);
