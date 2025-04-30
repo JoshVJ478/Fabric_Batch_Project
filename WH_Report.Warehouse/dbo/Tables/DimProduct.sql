3c72f80b8bf8ca6abd609cfe350ce1f41470c47f
JoshVJ478 <josue.villegas@greybearddynamics.com>
2025-04-29 21:08:08 -0500

    Delete WH_Report.Warehouse directory

--- WH_Report.Warehouse/dbo/Tables/DimProduct.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[DimProduct] (
-
-	[ProductKey] int NULL, 
-	[ProductID_OLTP] int NULL, 
-	[ProductName] varchar(8000) NULL, 
-	[ProductCategory] varchar(8000) NULL, 
-	[ProductBrand] varchar(8000) NULL
-);
